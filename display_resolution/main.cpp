435936b99b12d475b778890b03542fe5b966c14b
mochenya <2827075398@qq.com>
2023-08-11 17:47:27 +0800

    修复问题

--- display_resolution/main.cpp
+++ display_resolution/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include "Windows.h"
 
 int main(int argc, char *argv[])
@@ -7,6 +8,18 @@ int main(int argc, char *argv[])
   if (argc != 2)
   {
     std::cout << "Usage: " << "2/4" << "set display" << std::endl;
+    // 获取当前显示器列表
+    DISPLAY_DEVICEA displayDevice;
+    displayDevice.cb = sizeof(DISPLAY_DEVICEA);
+    for (DWORD i = 0; EnumDisplayDevicesA(NULL, i, &displayDevice, 0); i++)
+    {
+      std::cout << "Device Name: " << displayDevice.DeviceName << std::endl;
+      std::cout << "Device String: " << displayDevice.DeviceString << std::endl;
+      std::cout << "Device ID: " << displayDevice.DeviceID << std::endl;
+      std::cout << "Device Key: " << displayDevice.DeviceKey << std::endl;
+      std::cout << "State Flags: " << displayDevice.StateFlags << std::endl;
+      std::cout << std::endl;
+    }
     return 0;
   }
 
@@ -20,17 +33,17 @@ int main(int argc, char *argv[])
 
   // change display settings
   std::cout << "param: " << argv[1] << std::endl;
-  if (std::strcmp(argv[1], "4") == 0)
+  if (strcmp(argv[1], "4") == 0)
   {
     devMode.dmPelsWidth = 3840;  // set width
     devMode.dmPelsHeight = 2160; // set height
-  } else if (std::strcmp(argv[1], "2") == 0)
+  } else if (strcmp(argv[1], "2") == 0)
   {
     devMode.dmPelsWidth = 2560;  // set width
     devMode.dmPelsHeight = 1440; // set height
   } else
   {
-    std::cout << "Usage: " << "2/4" << "set display" << std::endl;
+    std::cout << "Usage: 2/4 set display" << std::endl;
     return 1;
   }
   devMode.dmFields = DM_PELSWIDTH | DM_PELSHEIGHT; // set fields
